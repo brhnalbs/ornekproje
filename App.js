c4e3670211ff1c1e574d7cca7a66e8e461afa8f7
brhnalbs <34123434+brhnalbs@users.noreply.github.com>
2017-12-19 20:28:35 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+//import ettiğimiz bölüm
+
+import React, { Component } from 'react';
+import {
+
+} from 'react-native';
+import Header from 'ornekproje/src/components/Header';
+
+
+//render ettiğimiz bölüm
+export default class App extends Component<{}> {
+  render() {
+    return (
+
+      <Header headerText={'Burhan Proje'} />
+
+    );
+  }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+//import
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+    <Text style={textStyle}> {props.headerText} </Text>
+    </View>
+);
+};
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#fefefe',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    shadowOffset: { widht: 0, height: 2 },
+    shadowOpacity: 0.5,
+  }
+};
+
+export default Header;
